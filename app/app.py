e20b8d2a7d363e88ce84d889c50ee34038e7633e
Darshit-27 <darshitpatel2705@gmail.com>
2026-04-15 00:13:37 +0530

    Initial commit: DevSecOps app + Docker setup

---
+++ app/app.py
@@ -0,0 +1,15 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "DevSecOps App Running 🚀"
+
+@app.route('/login')
+def login():
+    username = request.args.get('username')
+    return f"Welcome {username}"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
